01208770ae76b7455d533c01550c591b1944eed2
BBsan2k <bbsan@gmx.de>
2023-01-01 10:59:37 +0100

    Tweak Card change timing

--- src/ps1/ps1_odeman.c
+++ src/ps1/ps1_odeman.c
@@ -7,7 +7,7 @@
 #include <gui.h>
 #include <string.h>
 
-#define CARD_SWITCH_DELAY_MS    (120)
+#define CARD_SWITCH_DELAY_MS    (250)
 
 
 void ps1_odeman_init(void) {
@@ -24,88 +24,39 @@ void ps1_odeman_task(void) {
 
         switch (ode_command) {
             case MCP_GAME_ID:
-                printf("Received Game ID");
+                debug_printf("Received Game ID: ");
                 const char *game_id;
                 game_id = ps1_memory_card_get_game_id();
-                printf("PS1 Game ID Received: %s\n", game_id);
+                debug_printf("%s\n", game_id);
                 ps1_cardman_set_gameid(game_id);
                 break;
             case MCP_NXT_CARD:
+                debug_printf("Received next card.\n");
                 ps1_cardman_next_idx();
                 break;
             case MCP_PRV_CARD:
+                debug_printf("Received prev card.\n");
                 ps1_cardman_prev_idx();
                 break;
             case MCP_NXT_CH:
+                debug_printf("Received next chan.\n");
                 ps1_cardman_next_channel();
                 break;
             case MCP_PRV_CH:
+                debug_printf("Received prev chan.\n");
                 ps1_cardman_prev_channel();
                 break;
             default:
-                printf("Invalid ODE Command received.");
+                debug_printf("Invalid ODE Command received.");
                 break;
         }
 
         sleep_ms(CARD_SWITCH_DELAY_MS); // This delay is required, so ODE can register the card change
 
+
         ps1_cardman_open();
         ps1_memory_card_enter();
         gui_request_refresh();
     }
 
-    /*
-
-    if (mc_pro_flags&MCP_GAME_ID) {
-        char game_id[0xFF];
-        char cleaned_game_id[0x10] = { 0x00 };
-        ps1_memory_card_get_game_id(game_id);
-        debug_printf("PS1 Game ID Received: %s\n", game_id);
-        clean_title_id(game_id, cleaned_game_id);
-        debug_printf("Cleaned Game ID is %s\n", cleaned_game_id);
-        ps1_memory_card_exit();
-        ps1_cardman_close();
-        ps1_cardman_set_gameid(cleaned_game_id);
-        sleep_ms(CARD_SWITCH_DELAY_MS);
-        ps1_cardman_open();
-        ps1_memory_card_enter();
-        gui_request_refresh();
-    }
-    if (mc_pro_flags&MCP_NXT_CH) {
-        ps1_memory_card_exit();
-        ps1_cardman_close();
-        ps1_cardman_next_channel();
-        sleep_ms(CARD_SWITCH_DELAY_MS);
-        ps1_cardman_open();
-        ps1_memory_card_enter();
-        gui_request_refresh();
-    }
-    if (mc_pro_flags&MCP_PRV_CH) {
-        ps1_memory_card_exit();
-        ps1_cardman_close();
-        ps1_cardman_prev_channel();
-        sleep_ms(CARD_SWITCH_DELAY_MS);
-        ps1_cardman_open();
-        ps1_memory_card_enter();
-        gui_request_refresh();
-    }
-    if (mc_pro_flags&MCP_NXT_CARD) {
-        ps1_memory_card_exit();
-        ps1_cardman_close();
-        ps1_cardman_next_idx();
-        sleep_ms(CARD_SWITCH_DELAY_MS);
-        ps1_cardman_open();
-        ps1_memory_card_enter();
-        gui_request_refresh();
-    }
-    if (mc_pro_flags&MCP_PRV_CARD) {
-        ps1_memory_card_exit();
-        ps1_cardman_close();
-        ps1_cardman_prev_idx();
-        sleep_ms(CARD_SWITCH_DELAY_MS);
-        ps1_cardman_open();
-        ps1_memory_card_enter();
-        gui_request_refresh();
-    }
-    */
 }
