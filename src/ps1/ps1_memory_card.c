c63cd1516f66be8fca541eaae9cc60b49b7f2600
BBsan2k <bbsan@gmx.de>
2023-01-01 10:57:30 +0100

    Make Clean Func Time Critical

--- src/ps1/ps1_memory_card.c
+++ src/ps1/ps1_memory_card.c
@@ -32,10 +32,10 @@ typedef struct {
 
 static pio_t cmd_reader, dat_writer;
 static volatile int mc_exit_request, mc_exit_response, mc_enter_request, mc_enter_response;
-static void clean_title_id(const uint8_t* const in_title_id, char* const out_title_id, const size_t in_title_id_length);
+static void __time_critical_func(clean_title_id)(const uint8_t* const in_title_id, char* const out_title_id, const size_t in_title_id_length);
 
 
-static void clean_title_id(const uint8_t* const in_title_id, char* const out_title_id, const size_t in_title_id_length) {
+static void __time_critical_func(clean_title_id)(const uint8_t* const in_title_id, char* const out_title_id, const size_t in_title_id_length) {
     int idx_in_title = 0, idx_out_title = 0;
 
     while ( (in_title_id[idx_in_title] != 0x00) 
