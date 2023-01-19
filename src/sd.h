9c3ac6e24062157a0c83038c046ddf6a35d5bf19
BBsan2k <bbsan@gmx.de>
2023-01-19 16:33:33 +0100

    wip-ps2 exploit

--- src/sd.h
+++ src/sd.h
@@ -9,5 +9,6 @@ void sd_flush(int fd);
 int sd_read(int fd, void *buf, size_t count);
 int sd_write(int fd, void *buf, size_t count);
 int sd_seek(int fd, uint64_t pos);
+int sd_filesize(int fd);
 int sd_mkdir(const char *path);
 int sd_exists(const char *path);
