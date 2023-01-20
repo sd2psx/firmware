baf7afa8b11387deac12dfabb3f2f6063157de45
BBsan2k <bbsan@gmx.de>
2023-01-20 11:25:38 +0100

    Fix wrong pointer to buffer

--- src/main.c
+++ src/main.c
@@ -122,7 +122,7 @@ int main() {
 
         printf("Starting memory card... ");
         uint64_t start = time_us_64();
-        //gui_do_ps2_card_switch();
+        gui_do_ps2_card_switch();
         uint64_t end = time_us_64();
         printf("DONE! (%d us)\n", (int)(end - start));
 
