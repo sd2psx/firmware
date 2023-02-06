8acf5f03c2d9869abc4c8e45078f567eff465a9d
BBsan2k <bbsan@gmx.de>
2023-02-06 07:02:33 +0100

    Update game name processing

--- src/game_names/game_names.c
+++ src/game_names/game_names.c
@@ -1,32 +1,43 @@
 
 #include "game_names.h"
 
+
 #include <ctype.h>
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "pico/platform.h"
+
 
 #include "../debug.h"
 #include "../sd.h"
 #include "../settings.h"
 
 #define MAX_GAME_NAME_LENGTH (127)
-#define MAX_PREFIX_LENGTH    (4)
+#define MAX_PREFIX_LENGTH    (5)
 #define MAX_GAME_ID_LENGTH   (16)
+#define MAX_STRING_ID_LENGTH (10)
+#define MAX_PATH_LENGTH      (64)
 
 extern const char _binary_gamedbps1_dat_start, _binary_gamedbps1_dat_size;
 // extern const char _binary_gamedbps2_dat_start, _binary_gamedbps2_dat_size;
 
-static int card_idx;
-static int card_chan;
-static char card_game_id[MAX_GAME_ID_LENGTH];
+typedef struct {
+    size_t offset;
+    uint32_t game_id;
+    uint32_t parent_id;
+    const char* name;
+} game_lookup;
 
-static bool game_names_sanity_check_game_id(const char* const game_id) {
+bool __time_critical_func(game_names_sanity_check_title_id)(const char* const title_id) {
     uint8_t i = 0U;
 
     char splittable_game_id[MAX_GAME_ID_LENGTH];
-    strlcpy(splittable_game_id, game_id, MAX_GAME_ID_LENGTH);
+    strlcpy(splittable_game_id, title_id, MAX_GAME_ID_LENGTH);
     char* prefix = strtok(splittable_game_id, "-");
     char* id = strtok(NULL, "-");
 
@@ -81,87 +92,193 @@ static uint32_t game_names_find_prefix_offset(uint32_t numericPrefix, const char
     return offset;
 }
 
-const char* game_names_get_game_name(const char* const id) {
-    char prefixString[MAX_PREFIX_LENGTH + 1] = {};
+static game_lookup build_game_lookup(const char* const db_start, const size_t db_size, const size_t offset) {
+    game_lookup game = {};
+    size_t name_offset;
+    game.game_id = game_names_char_array_to_uint32(&(db_start)[offset]);
+    game.offset = offset;
+    game.parent_id = game_names_char_array_to_uint32(&(db_start)[offset + 8]);
+    name_offset = game_names_char_array_to_uint32(&(db_start)[offset + 4]);
+    if ((name_offset < db_size) && ((db_start)[name_offset] != 0x00))
+        game.name = &((db_start)[name_offset]);
+    else
+        game.name = NULL;
+
+    return game;
+}
+
+static bool file_has_extension(const char* const file, const char* const extension) {
+    int file_index = strlen(file) - 1;
+    int ext_index = strlen(extension) - 1;
+    while ((file_index >= 0) && (ext_index >= 0))
+        if (file[file_index--] != extension[ext_index--])
+            return false;
+
+    return true;
+}
+
+static void file_remove_extension(char* const file) {
+    int file_index = strlen(file) - 1;
+    while ((file_index >= 0) && (file[file_index] != '.')) {
+        file[file_index--] = 0x00;
+    }
+    if (file_index >= 0)
+        file[file_index] = 0x00;
+}
+
+static game_lookup find_game_lookup(const char* game_id) {
+    char prefixString[MAX_PREFIX_LENGTH] = {};
     char idString[10] = {};
-    const char* card_game_name = NULL;
+    uint32_t numeric_id = 0, numeric_prefix = 0;
 
     //    const char* const db_start = settings_get_mode() == MODE_PS1 ? &_binary_gamedbps1_dat_start : &_binary_gamedbps2_dat_start;
     //    const char* const db_size = settings_get_mode() == MODE_PS1 ? &_binary_gamedbps1_dat_size : &_binary_gamedbps2_dat_size;
 
     const char* const db_start = &_binary_gamedbps1_dat_start;
-    const char* const db_size = &_binary_gamedbps1_dat_size;
+    const size_t db_size = (size_t)&_binary_gamedbps1_dat_size;
 
-    uint32_t numericPrefix = 0, prefixOffset = 0, currentId = 0, numericId = 0;
+    uint32_t prefixOffset = 0;
+    game_lookup ret = {
+        .game_id = 0U,
+        .parent_id = 0U,
+        .name = NULL
+    };
 
-    if (game_names_sanity_check_game_id(id)) {
-        char* copy = strdup(id);
+
+    if (game_id != NULL && game_id[0]) {
+        char* copy = strdup(game_id);
         char* split = strtok(copy, "-");
 
         if (strlen(split) > 0) {
-            strlcpy(prefixString, split, MAX_PREFIX_LENGTH + 1);
-            for (uint8_t i = 0; i < MAX_PREFIX_LENGTH; i++) {
+            strlcpy(prefixString, split, MAX_PREFIX_LENGTH);
+            for (uint8_t i = 0; i < MAX_PREFIX_LENGTH - 1; i++) {
                 prefixString[i] = toupper((unsigned char)prefixString[i]);
             }
-            numericPrefix = game_names_char_array_to_uint32(prefixString);
         }
 
         split = strtok(NULL, "-");
 
         if (strlen(split) > 0) {
             strlcpy(idString, split, 11);
-            numericId = atoi(idString);
+            numeric_id = atoi(idString);
         }
+    }
 
-        prefixOffset = game_names_find_prefix_offset(numericPrefix, db_start);
+    numeric_prefix = game_names_char_array_to_uint32(prefixString);
 
-        if (prefixOffset < (size_t)db_size) {
+    if (numeric_id != 0) {
+        
+        prefixOffset = game_names_find_prefix_offset(numeric_prefix, db_start);
+
+        if (prefixOffset < db_size) {
             uint32_t offset = prefixOffset;
+            game_lookup game;
             do {
-                currentId = game_names_char_array_to_uint32(&(db_start)[offset]);
-                if (currentId == numericId) {
-                    uint32_t name_offset = game_names_char_array_to_uint32(&(db_start)[offset + 4]);
+                game = build_game_lookup(db_start, db_size, offset);
 
-                    debug_printf("Found ID - Name Offset: %d, Parent ID: %d\n", (int)name_offset,
-                                 (int)game_names_char_array_to_uint32(&(db_start)[offset + 8]));
+                if (game.game_id == numeric_id) {
+                    ret = game;
+                    debug_printf("Found ID - Name Offset: %d, Parent ID: %d\n", (int)game.name, game.parent_id);
+                    debug_printf("Name:%s\n", game.name);
 
-                    snprintf(card_game_id, MAX_GAME_ID_LENGTH, "%s-%0*d", prefixString, (int)strlen(idString),
-                             (int)game_names_char_array_to_uint32(&(db_start)[offset + 8]));
-
-                    debug_printf("Parent ID: %s\n", card_game_id);
-
-                    if ((name_offset < (size_t)db_size) && (db_start + name_offset) != 0x00) {
-                        card_game_name = (db_start + name_offset);
-                        debug_printf("Name:%s\n", card_game_name);
-
-                        return card_game_name;
-                    } else {
-                        return NULL;
-                    }
                 }
                 offset += 12;
-            } while (currentId != 0);
+            } while ((game.game_id != 0) && (offset < db_size) && (ret.game_id == 0));
         }
     }
 
-    return NULL;
+    return ret;
 }
 
-const char* game_names_name_from_file(char* path, size_t size, int channel) {
-    int dir, it;
-    char filename[MAX_GAME_NAME_LENGTH] = {};
+void __time_critical_func(game_names_extract_title_id)(const uint8_t* const in_title_id, char* const out_title_id, const size_t in_title_id_length, const size_t out_buffer_size) {
+    uint16_t idx_in_title = 0, idx_out_title = 0;
 
-    dir = sd_open(path, O_RDONLY);
-    if (dir >= 0) {
-        it = sd_iterate_dir(dir, it);
-        while (it != -1) {
-            int i = MAX_GAME_NAME_LENGTH - 1;
-            sd_get_name(it, filename, MAX_GAME_NAME_LENGTH);
-            while ((filename[i] == 0x0) && (i > 0))
-                i--;
-            if ((i > 4) && (filename[i--] == 't') && (filename[i--] == 'x') && (filename[i--] == 't')) {
+    while ( (in_title_id[idx_in_title] != 0x00) 
+            && (idx_in_title < in_title_id_length)
+            && (idx_out_title < out_buffer_size) ) {
+        if ((in_title_id[idx_in_title] == ';') || (in_title_id[idx_in_title] == 0x00)) {
+            out_title_id[idx_out_title++] = 0x00;
+            break;
+        } else if ((in_title_id[idx_in_title] == '\\') || (in_title_id[idx_in_title] == '/') || (in_title_id[idx_in_title] == ':')) {
+            idx_out_title = 0;
+        } else if (in_title_id[idx_in_title] == '_') {
+            out_title_id[idx_out_title++] = '-';
+        } else if (in_title_id[idx_in_title] != '.') {
+            out_title_id[idx_out_title++] = in_title_id[idx_in_title];
+        } else {
+        }
+        idx_in_title++;
+    }
+}
 
+void game_names_get_name_by_folder(const char* const folder, char* const game_name) {
+    if (game_names_sanity_check_title_id(folder)) {
+        game_lookup game;
+
+        game = find_game_lookup(folder);
+
+        if (game.game_id != 0) {
+            strlcpy(game_name, game.name, MAX_GAME_NAME_LENGTH);
+        }
+    } else {
+        int dir_fd, it_fd = -1;
+        char filename[MAX_GAME_NAME_LENGTH] = {};
+        char dir[64];
+        if (settings_get_mode() == MODE_PS1) {
+            snprintf(dir, sizeof(dir), "MemoryCards/PS1/%s", folder);
+        } else {
+            snprintf(dir, sizeof(dir), "MemoryCards/PS2/%s", folder);
+        }
+
+        dir_fd = sd_open(dir, O_RDONLY);
+        if (dir_fd >= 0) {
+            it_fd = sd_iterate_dir(dir_fd, it_fd);
+
+            while (it_fd != -1) {
+                sd_get_name(it_fd, filename, MAX_GAME_NAME_LENGTH);
+
+                if (file_has_extension(filename, ".txt")) {
+                    file_remove_extension(filename);
+                    strlcpy(game_name, filename, MAX_GAME_NAME_LENGTH);
+                    break;
+                }
+                it_fd = sd_iterate_dir(dir_fd, it_fd);
+            }
+            if (it_fd != -1)
+                sd_close(it_fd);
+            sd_close(dir_fd);
+        }
+    }
+}
+
+void game_names_get_parent(const char* const game_id, char* const parent_id) {
+    char prefixString[MAX_PREFIX_LENGTH] = {};
+    char idString[10] = {};
+    game_lookup game;
+
+    if (game_id != NULL && game_id[0]) {
+        char* copy = strdup(game_id);
+        char* split = strtok(copy, "-");
+
+        if (strlen(split) > 0) {
+            strlcpy(prefixString, split, MAX_PREFIX_LENGTH);
+            for (uint8_t i = 0; i < MAX_PREFIX_LENGTH - 1; i++) {
+                prefixString[i] = toupper((unsigned char)prefixString[i]);
             }
         }
+        split = strtok(NULL, "-");
+
+        if (strlen(split) > 0) {
+            strlcpy(idString, split, 11);
+        }
+    }
+
+    game = find_game_lookup(game_id);
+
+    if (game.game_id != 0) {
+        snprintf(parent_id, MAX_GAME_ID_LENGTH, "%s-%0*d", prefixString, (int)strlen(idString), (int)game.parent_id);
+
+        debug_printf("Parent ID: %s\n", parent_id);
     }
 }
+
--- src/game_names/game_names.h
+++ src/game_names/game_names.h
@@ -1,3 +1,20 @@
 #pragma once
 
-const char* game_names_get_game_name(const char* const id);
+#include <stddef.h>
+#include <stdint.h>
+#include <pico/platform.h>
+
+void game_names_extract_title_id(const uint8_t* const in_title_id, char* const out_title_id, const size_t in_title_id_length, const size_t out_buffer_size);
+bool game_names_sanity_check_title_id(const char* const title_id);
+
+void game_names_get_name_by_folder(const char* const folder, char* const game_name);
+void game_names_get_parent(const char* const game_id, char* const parent_id);
+
+void game_names_update_card_dir(const char* const dir);
+void game_names_init(void);
+
+void game_names_set_game_id(const char* const game_id);
+const char* game_names_get_game_id(void);
+
+const char* game_names_get_game_name(void);
+const char* game_names_name_from_file(char* path, int channel);
