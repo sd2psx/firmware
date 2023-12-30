f5442ebe0e6f9a5eb8f51510c6896909a1730b8f
BBsan2k <bbsan@gmx.de>
2023-12-30 12:03:29 +0100

    Implement set game ID

--- src/ps2/card_emu/ps2_sd2psxman.c
+++ src/ps2/card_emu/ps2_sd2psxman.c
@@ -1,27 +1,27 @@
-#include <stdio.h>
-#include "ps2/ps2_cardman.h"
-#include "ps2/card_emu/ps2_memory_card.h"
 #include "ps2/card_emu/ps2_sd2psxman.h"
-#include "gui.h"
-#include "debug.h"
 
+#include <stdio.h>
+#include <string.h>
+
+#include "debug.h"
+#include "gui.h"
 #include "pico/time.h"
+#include "ps2/card_emu/ps2_memory_card.h"
 #include "ps2/card_emu/ps2_sd2psxman_commands.h"
+#include "ps2/ps2_cardman.h"
 
 volatile uint8_t sd2psxman_cmd;
 volatile uint8_t sd2psxman_mode;
 volatile uint16_t sd2psxman_cnum;
-char sd2psxman_gameid[251] = { 0x00 };
+char sd2psxman_gameid[251] = {0x00};
 
-void ps2_sd2psxman_task(void)
-{
+void ps2_sd2psxman_task(void) {
     if (sd2psxman_cmd != 0) {
-
         uint16_t prev_card = ps2_cardman_get_idx();
         uint8_t prev_chan = ps2_cardman_get_channel();
- 
+        ps2_cardman_state_t prev_state = ps2_cardman_get_state();
+
         switch (sd2psxman_cmd) {
-            
             case SD2PSXMAN_SET_CARD:
                 if (sd2psxman_mode == SD2PSXMAN_MODE_NUM) {
                     ps2_cardman_set_idx(sd2psxman_cnum);
@@ -47,32 +47,40 @@ void ps2_sd2psxman_task(void)
                     debug_printf("set prev channel\n");
                 }
                 break;
-            
-            case SD2PSXMAN_SET_GAMEID:
-                //TODO:
-                break;
 
+            case SD2PSXMAN_SET_GAMEID: 
+                    ps2_cardman_set_gameid(sd2psxman_gameid); 
+                    debug_printf("set next channel\n");
+                break;
             case SD2PSXMAN_UNMOUNT_BOOTCARD:
                 if (ps2_cardman_get_idx() == 0) {
                     ps2_cardman_next_idx();
                 }
                 break;
 
-            default:
-            break;
+            default: break;
         }
 
-        if (prev_card != ps2_cardman_get_idx() || prev_chan != ps2_cardman_get_channel()) {
-            //close old card
+        if (prev_card != ps2_cardman_get_idx() || prev_chan != ps2_cardman_get_channel() || (prev_state != ps2_cardman_get_state()) ||
+            (SD2PSXMAN_SET_GAMEID == sd2psxman_cmd)) {
+            // close old card
             ps2_memory_card_exit();
             ps2_cardman_close();
 
-            //open new card
+            // open new card
             gui_do_ps2_card_switch();
-            //ps2_memory_card_enter();
+            // ps2_memory_card_enter();
             gui_request_refresh();
         }
 
         sd2psxman_cmd = 0;
     }
+}
+
+void __time_critical_func(ps2_sd2psxman_set_gameid)(const char* const game_id) {
+    snprintf(sd2psxman_gameid, sizeof(sd2psxman_gameid), "%s", game_id);
+}
+
+const char* ps2_sd2psxman_get_gameid(void) {
+    return sd2psxman_gameid;
 }
--- src/ps2/card_emu/ps2_sd2psxman.h
+++ src/ps2/card_emu/ps2_sd2psxman.h
@@ -8,3 +8,5 @@ extern volatile uint16_t sd2psxman_cnum;
 extern char sd2psxman_gameid[251];
 
 void ps2_sd2psxman_task(void);
+void ps2_sd2psxman_set_gameid(const char* const game_id);
+const char* ps2_sd2psxman_get_gameid(void);
